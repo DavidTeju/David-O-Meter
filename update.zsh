64df907cb56421c8afbcc8dd2db5cc53a1c82f17
David Tejuosho <tejuoshodavid@gmail.com>
2022-04-24 15:59:55 -0500

    update values

---
+++ update.zsh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+cd /Users/tejuo/Library/CloudStorage/OneDrive-Personal/Programming/IntelliJ/David-O-Meter || exit
+git add "sentimentValues.json"
+git commit -m "update values"
+git push
+exit
