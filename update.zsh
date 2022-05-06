90962a9077a9c3959941d45ed42490df9ab76309
David Tejuosho <tejuoshodavid@gmail.com>
2022-05-06 03:19:02 -0500

    script now updates logs and pulls before pushes

--- update.zsh
+++ update.zsh
@@ -1,5 +1,9 @@
 #!/bin/zsh
 cd /Users/tejuo/Library/CloudStorage/OneDrive-Personal/Programming/IntelliJ/David-O-Meter || exit
 git add "sentimentValues.json"
+git add "gitUpdates.log"
+git add "tweetsAnalysed.log"
 git commit -m "update values"
+git pull
 git push
+exit
