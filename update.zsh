535970730d6937babe59c15c5e668f195e980591
David Tejuosho <tejuoshodavid@gmail.com>
2022-05-06 03:22:24 -0500

    small change

--- update.zsh
+++ update.zsh
@@ -1,8 +1,6 @@
 #!/bin/zsh
 cd /Users/tejuo/Library/CloudStorage/OneDrive-Personal/Programming/IntelliJ/David-O-Meter || exit
-git add "sentimentValues.json"
-git add "gitUpdates.log"
-git add "tweetsAnalysed.log"
+git add "sentimentValues.json" "gitUpdates.log" "tweetsAnalysed.log"
 git commit -m "update values"
 git pull
 git push
